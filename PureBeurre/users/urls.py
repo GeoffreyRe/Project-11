92aa6d131b59810ec90eec33d5611993dc645509
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2020-06-08 09:42:38 +0200

    correction du bug de connexion des utilisateurs

--- PureBeurre/users/urls.py
+++ PureBeurre/users/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('register/', views.sign_up, name="register"),
+    path('register/', views.sign_up, name="signup"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name='logout'),
     url(
