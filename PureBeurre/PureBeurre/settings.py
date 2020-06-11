edee681522a9dce136df973d4e391a761e957c63
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2020-06-11 11:50:39 +0200

    ajout d'une variable d'environnement heroku

--- PureBeurre/PureBeurre/settings.py
+++ PureBeurre/PureBeurre/settings.py
@@ -163,7 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'purbeurre.geoffrey.remacle@gmail.com'
-EMAIL_HOST_PASSWORD = 'purbeurre4589'
+EMAIL_HOST_PASSWORD = os.environ.get("PASSWORD_EMAIL")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
